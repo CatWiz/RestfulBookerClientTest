138b2dde5bf1b4d2b39be1380379be7bec2e37ef
CatWizard <thecatwizard124@gmail.com>
2025-03-03 21:30:43 +0200

    added Kiota client

--- RestfulBookerAPIClient/Program.cs
+++ RestfulBookerAPIClient/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using KiotaPosts.RestfulBookerClient;
+using KiotaPosts.RestfulBookerClient.Models;
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
 
-Console.WriteLine("Hello, World!");
+var authProvider = new AnonymousAuthenticationProvider();
+var adapter = new HttpClientRequestAdapter(authProvider);
+adapter.BaseUrl = "https://restful-booker.herokuapp.com";
+var client = new RestfulBookerClient(adapter);
+
+try
+{
+    string? pingResponse = await client.Ping.GetAsync();
+    Console.WriteLine($"Ping response: {pingResponse}");
+
+    var userId = 1;
+    var someBooking = await client.Booking[userId].GetAsync();
+    if (someBooking is not null)
+    {
+        Console.WriteLine($"Found a booking for user {userId}:");
+        Console.WriteLine($"{someBooking.Firstname} {someBooking.Lastname}");
+        Console.WriteLine($"{someBooking.Bookingdates?.Checkin} - {someBooking.Bookingdates?.Checkout}");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    Console.WriteLine(e.StackTrace);
+}
