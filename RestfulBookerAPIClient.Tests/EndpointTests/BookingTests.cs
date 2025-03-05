1b2160d07af044bbe6a58c9d0467b364676b7197
CatWizard <thecatwizard124@gmail.com>
2025-03-05 08:57:01 +0200

    get all/get one existing booking tests

---
+++ RestfulBookerAPIClient.Tests/EndpointTests/BookingTests.cs
@@ -0,0 +1,42 @@
+﻿using KiotaPosts.RestfulBookerClient;
+using RestfulBookerAPIClient.Tests.Fixtures;
+
+namespace RestfulBookerAPIClient.Tests.PingTests;
+
+[Collection("ApiClient")]
+public class BookingTests
+{
+    private readonly RestfulBookerClient _client;
+    
+    public BookingTests(ApiClientFixture fixture)
+    {
+        _client = fixture.Client;
+    }
+    
+    [Fact]
+    public async Task GetAllBookingsReturnsOk()
+    {
+        var bookingsList = await _client.Booking.GetAsync();
+        
+        Assert.NotNull(bookingsList);
+        Assert.NotEmpty(bookingsList);
+        Assert.NotNull(bookingsList.First().Bookingid);
+    }
+    
+    [Fact]
+    public async Task GetBookingByIdReturnsOk()
+    {
+        var bookingsList = await _client.Booking.GetAsync();
+        var bookingId = bookingsList?.First().Bookingid;
+        Assert.NotNull(bookingId);
+        
+        var booking = await _client.Booking[(int)bookingId].GetAsync();
+        
+        Assert.NotNull(booking);
+        Assert.NotNull(booking.Firstname);
+        Assert.NotNull(booking.Lastname);
+        Assert.NotNull(booking.Bookingdates);
+        Assert.NotNull(booking.Bookingdates.Checkin);
+        Assert.NotNull(booking.Bookingdates.Checkout);
+    }
+}
